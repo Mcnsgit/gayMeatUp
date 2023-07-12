ff72ea3c794483fb9acf43ec6040e61b400bf866
mcnsgit <cardigamiguel221@gmail.com>
2023-07-12 11:57:55 +0100

    dev

--- gayMeatUp/gayMeatUP/App.tsx
+++ gayMeatUp/gayMeatUP/App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.tsx
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomePage from './src/pages/homePage';
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={{ color: '#fff' }}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Home" component={HomePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
